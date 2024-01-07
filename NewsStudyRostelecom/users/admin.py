85e91c92ab5d06a271a6d8cc5cd4d5ea6ce21cc3
PavelSkobeev <pavel_skobeev@mail.ru>
2024-01-07 19:58:18 +0300

    поправил цвет ошибки при регистрации, добавил фото пользователя в админке

--- NewsStudyRostelecom/users/admin.py
+++ NewsStudyRostelecom/users/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ['user', 'gender', 'birthdate', 'account_image']
+    list_display = ['user', 'gender', 'birthdate', 'get_account_image']
     list_filter = ['user', 'birthdate']
+
+    def get_account_image(self, obj):
+        return mark_safe(f'<img src={obj.account_image.url} width="50px", height="auto"')
+
+    get_account_image.shrt_description = 'Изображение'
 
 admin.site.register(Account, AccountAdmin)
