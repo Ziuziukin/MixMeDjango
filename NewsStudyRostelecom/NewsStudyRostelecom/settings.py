d0be67696215ded614187c1ad7ad1cebf29d084d
PavelSkobeev <pavel_skobeev@mail.ru>
2024-01-08 23:55:31 +0300

    add to gitignore

--- NewsStudyRostelecom/NewsStudyRostelecom/settings.py
+++ NewsStudyRostelecom/NewsStudyRostelecom/settings.py
@@ -130,7 +130,6 @@ STATICFILES_DIRS = [
 import os
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-print(MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
